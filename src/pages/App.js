3caef9fdbbaff4cdc97a36f886c809b70ef23730
THGPtrading <ira@thgptrading.com>
2025-11-11 01:07:53 -0700

    Update and rename src/App.js to src/pages/App.js

---
+++ src/pages/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import LightweightChart from '../LightweightChart';
+import SetupFeed from '../components/setupFeed';
+import { fetchPolygonData } from '../api/polygon';
+
+const App = () => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    fetchPolygonData('AAPL').then(setChartData);
+  }, []);
+
+  return (
+    <div style={{ padding: '2rem', color: '#ccc' }}>
+      <h2>Openedge Dashboard</h2>
+      <p>Scalp-focused ICC charting platform with ChartEye overlays</p>
+      <LightweightChart data={chartData} />
+      <hr />
+      <SetupFeed />
+    </div>
+  );
+};
+
+export default App;
