a3ce56029a7c32b35260c7c73a0ea18b7dcde242
THGPtrading <ira@thgptrading.com>
2025-11-11 00:03:47 -0700

    Update polygon.js

--- src/api/polygon.js
+++ src/api/polygon.js
@@ -1,6 +1,7 @@
 // Basic Polygon.io fetch using free API key
 
-const API_KEY = '63f1695e-c099-4934-90d4-bb352370fbb5';
+const API_KEY = 'const API_KEY = process.env.POLYGON_API_KEY;
+';
 
 export const fetchPolygonData = async (ticker = 'AAPL') => {
   const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/minute/2023-01-01/2023-01-01?apiKey=${API_KEY}`;
